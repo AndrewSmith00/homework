4edcedc97705963d9de68741f858ab8fd8507a0a
AndrewSmith00 <91675747+AndrewSmith00@users.noreply.github.com>
2022-03-15 18:47:11 +0300

    Add files via upload

---
+++ MostFrequentSymbol/stringLib.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+int my_strlen(char* str1);
+char* my_strcat(char* str1, char* str2);
+char* my_strcpy(char* str1, char* str2);
+int my_strcmp(char* str1, char* str2);
+char* my_strchr(char* str1, char smb);
+char* my_strstr(char* str1, char* str2);
+char* my_strdel(char* str1, int start, int end);
+char* my_strrev(char* str1);
