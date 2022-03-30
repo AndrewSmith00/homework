97ae50c753aca1c46f63bc20cd7ceceba845084c
AndrewSmith00 <91675747+AndrewSmith00@users.noreply.github.com>
2022-03-30 21:17:00 +0300

    Update Source.cpp

--- Удаление_чётных_слов_переворот_нечётных/Source.cpp
+++ Удаление_чётных_слов_переворот_нечётных/Source.cpp
@@ -5,57 +5,54 @@
 using namespace std;
 
 char* getWord(char* str);
-char* getNewString(char* str);
+void getNewString(char* str);
 
 int main()
 {
-	char str[999] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elit ex, posuere et diam id, imperdiet dictum nulla. Donec enim libero, ullamcorper sed justo ut, lacinia pharetra purus. Aliquam et massa commodo, mollis urna vitae, varius quam. Vivamus vel lobortis ex, id pharetra est.";
-	char* word = getWord(str);
-	char* newString = getNewString(str);
-
-	printf("%s", newString);
+	char str[999] = "Lorem    ipsum dolor sit amet, consectetur adipiscing elit. Cras elit ex, posuere et diam id, imperdiet dictum nulla. Donec enim libero, ullamcorper sed justo ut, lacinia pharetra purus. Aliquam et massa commodo, mollis urna vitae, varius quam. Vivamus vel lobortis ex, id pharetra est.";
+	getNewString(str);
+	printf("%s", str);
 }
 
 char* getWord(char* str) {
-	char *word = new char[98];
+	char* word = new char[98];
 	char* wordStart = word;
-	for (; *str != '\0'; str++) {
-			for (; *str != ' '; str++, word++) {
-				if (*str == '\0') {
-					break;
-				}
-				*word = *str;
-			}
-			*word = '\0';
-			return wordStart;
+	while (*str != ' ') {
+		if (*str == '\0') {
+			break;
+		}
+		*word = *str;
+		str++; word++;
 	}
+	*word = '\0';
+	return wordStart;
 }
 
 
-char* getNewString(char* str) {
+void getNewString(char* str) {
 	char* newString = new char[98];
 	char* newStringStart = newString;
 	char* word = new char[98];
 	char* newWord = new char[97];
 	char* newWordStart = newWord;
 	int len = 0;
-	for (; *str != '\0'; str++) {
-		if (*str == ' ') {
-			*newString = *str;
-			newString++;
-			str++;
-		}
+	for (int i = 0; str[i]; i++) {
+		if (str[i] == ' ')
+			continue;
 
-		word = getWord(str);
+		word = getWord(str + i);
 		int len = my_strlen(word);
+		int n = 0;
 		if (len % 2 != 0) {
-			newWord = my_strrev(word);
-			for (; *newWord != '\0'; newWord++, newString++)
-				*newString = *newWord;
+			my_strrevInPlace(word);
+			while (n < len)
+			{
+				str[i] = word[n];
+				i++; n++;
+			}
 		}
-		str += len - 1;
+		else {
+			my_strdelInPlace(str, i, i + len - 1);
+		}
 	}
-	*newString = '\0';
-	return newStringStart;
 }
-
