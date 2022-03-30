b7e7aef7537c729146c172de954b180bc8692244
AndrewSmith00 <91675747+AndrewSmith00@users.noreply.github.com>
2022-03-31 00:13:29 +0300

    Update stringLib.h

--- Меню/stringLib.h
+++ Меню/stringLib.h
@@ -2,12 +2,12 @@
 
 
 int my_strlen(char* str1);
-char* my_strcat(char* str1, char* str2);
-char* my_strcpy(char* str1, char* str2);
+bool my_strcat(char* str1, char* str2);
+bool my_strcpy(char* str1, char* str2);
 int my_strcmp(char* str1, char* str2);
 char* my_strchr(char* str1, char smb);
 char* my_strstr(char* str1, char* str2);
 char* my_strdel(char* str1, int start, int end);
-void my_strdelInPlace(char* str, int k, int m);
+bool my_strdelInPlace(char* str, int k, int m);
 char* my_strrev(char* str1);
 void my_strrevInPlace(char* str);
