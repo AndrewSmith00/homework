56af9695a53278183699ec6d9d5df2bfc4b86571
AndrewSmith00 <91675747+AndrewSmith00@users.noreply.github.com>
2022-03-30 21:49:14 +0300

    Update stringLib.cpp

--- Меню/stringLib.cpp
+++ Меню/stringLib.cpp
@@ -3,10 +3,12 @@
 
 using namespace std;
 
-int my_strlen(char* str1)
+int my_strlen(char* str)
 {
 	int len = 0;
-	while (str1++) len++;
+	while (*str++) { 
+		len++; 
+	}
 	return len;
 }
 
@@ -16,10 +18,10 @@ char* my_strcat(char* str1, char* str2)
 
 	char* start = str1;
 	str1 += len1;
-	while (*str2 != '\0')
+	while (*str2++)
 	{
 		*str1 = *str2;
-		str1++; str2++;
+		str2++;
 	}
 	return start;
 }
@@ -27,9 +29,9 @@ char* my_strcat(char* str1, char* str2)
 char* my_strcpy(char* str1, char* str2)
 {
 	char* start = str2;
-	while (*str1 != '\0') {
+	while (*str1++) {
 		*str2 = *str1;
-		str1++, str2++;
+		str2++;
 	}
 	*str2 = '\0';
 	return start;
@@ -38,7 +40,7 @@ char* my_strcpy(char* str1, char* str2)
 int my_strcmp(char* str1, char* str2)
 {
 	int diff = 0;
-	while (*str1 !='\0' || *str2 != '\0')
+	while (*str1 || *str2)
 	{
 		if (*str1 != *str2)
 			diff += *str1 - *str2;
@@ -46,17 +48,17 @@ int my_strcmp(char* str1, char* str2)
 			diff += *str1;
 		if (*str1 == '\0')
 			diff -= *str2;
+		str1++; str2++;
 	}
 	return diff;
 }
 
 char* my_strchr(char* str1, char smb)
 {
-	while(*str1 != '0') 
+	while (*str1++)
 	{
 		if (*str1 == smb)
 			return str1;
-		str1++;
 	}
 	return NULL;
 }
@@ -65,22 +67,21 @@ char* my_strstr(char* str1, char* str2)
 {
 	char* start = NULL;
 
-	while (*str1 != '0')
+	while (*str1++)
 	{
 		if (*str1 == *str2)
 		{
 			start = str1;
-			while (*str2 != '0')
+			while (*str2++)
 			{
 				if (*str1 != *str2)
 				{
 					start = NULL;
 					break;
 				}
-				str1++; str2++;
+				str1++;
 			}
 		}
-		str1++;
 	}
 
 	return start;
@@ -107,7 +108,7 @@ char* my_strrev(char* str1)
 	char* start = str2;
 	int n = 0;
 	int len = strlen(str1);
-	while(len > 0)
+	while (len > 0)
 	{
 		str2[n] = str1[len - 1];
 		len--; n++;
@@ -129,8 +130,7 @@ void my_strrevInPlace(char* str) {
 	int n = 0;
 	int len = strlen(str);
 	int i = 0;
-	//if (len % 2 == 0) i = 1;
-	while (i < len/2)
+	while (i < len / 2)
 	{
 		char temp = str[len - 1];
 		str[len - 1] = str[n];
