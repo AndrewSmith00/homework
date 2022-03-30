2dafa847e2573aea91cf1aa9563afa5d29eb777f
AndrewSmith00 <91675747+AndrewSmith00@users.noreply.github.com>
2022-03-30 21:18:39 +0300

    Update stringLib.h

--- Удаление_чётных_слов_переворот_нечётных/stringLib.h
+++ Удаление_чётных_слов_переворот_нечётных/stringLib.h
@@ -8,4 +8,6 @@ int my_strcmp(char* str1, char* str2);
 char* my_strchr(char* str1, char smb);
 char* my_strstr(char* str1, char* str2);
 char* my_strdel(char* str1, int start, int end);
+void my_strdelInPlace(char* str, int k, int m);
 char* my_strrev(char* str1);
+void my_strrevInPlace(char* str);
