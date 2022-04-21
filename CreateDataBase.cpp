d32ee86ac3f6476569da826512ecb63ff980b344
AndrewSmith00 <91675747+AndrewSmith00@users.noreply.github.com>
2022-04-21 12:17:04 +0300

    Add files via upload

---
+++ CreateDataBase.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#pragma warning(disable : 4996) 
+
+using namespace std;
+
+struct Student
+{
+	char name[30];
+	char surname[30];
+	char secondname[30];
+	char adressStreet[30];
+	int adress, groupNumber, booksNumber, booksValue;
+};
+
+int setStudentInfo(char* fileName);
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	char fileName[100] = { "C:/Users/aokuznetsov/source/repos/BinaryFiles/BinaryFiles/source.bin" };
+
+	setStudentInfo(fileName);
+}
+
+int setStudentInfo(char* fileName)
+{
+	FILE* fPtr = fopen(fileName, "wb");
+	struct Student student;
+	int count;
+
+	if (!fPtr) {
+		printf("Ошибка при создании файла");
+		return -1;
+	}
+	else {
+		printf("Вводите данные о студентах. Для завершения введите - - - - 0 0 0 0\n");
+		scanf("%s%s%s%s%d%d%d%d", student.surname, student.name, student.secondname, student.adressStreet, &student.adress, &student.groupNumber, &student.booksNumber, &student.booksValue);
+
+		while(strcmp(student.surname, "-") != 0)
+		{
+			fwrite(&student, sizeof(struct Student), 1, fPtr);
+			scanf("%s%s%s%s%d%d%d%d", student.surname, student.name, student.secondname, student.adressStreet, &student.adress, &student.groupNumber, &student.booksNumber, &student.booksValue);
+		}
+		fclose(fPtr);
+		return 0;
+	}
+}
