fd1e865f02c64b687d397927f127a1a943394e5d
AndrewSmith00 <91675747+AndrewSmith00@users.noreply.github.com>
2022-04-27 21:23:52 +0300

    Update DeleteTagsFromXML.cpp

--- DeleteTags/DeleteTagsFromXML.cpp
+++ DeleteTags/DeleteTagsFromXML.cpp
@@ -1,135 +1,112 @@
-﻿#include <stdio.h>
-#pragma warning(disable : 4996) 
-
-void DeleteTags(char* fileName, char* tag);
-int len(char* str);
-void my_strcat(char* str1, const char* str2);
-char* substr(char* str1, char* str2);
-char* delSubstr(char* str1, int start, int end);
-
-int main()
-{
-    char fileName[99];
-    char tag[99];
-
-    scanf("%s", fileName);
-    scanf("%s", tag);
+#include <stdio.h>
+#include <stdlib.h>
 
 
-    DeleteTags(fileName, tag);
-}
-
-void DeleteTags(char* fileName, char* tag)
-{
-    FILE* fPtr;
-    char* buffer = new char[999];
-    char fullTag[99] = "<";
-    char closedTag[99] = "</";
-    char* tagPos;
-    char* closedTagPos;
-    int pos1, pos2;
-
-    fPtr = fopen(fileName, "r");
-
-    if (!fPtr) 
-    {
-        printf("File can not be opened");
-    }
-    else
-    {
-        my_strcat(fullTag, tag);
-        my_strcat(fullTag, ">\0");
-        printf("%s\n", fullTag);
-
-        my_strcat(closedTag, tag);
-        my_strcat(closedTag, ">\0");
-        printf("%s\n", closedTag);
-
-        while (fgets(buffer, 99, fPtr) != NULL) {
-            tagPos = substr(buffer, fullTag);
-            while (tagPos != NULL)
-            {
-                pos1 = tagPos - buffer;
-
-                closedTagPos = substr(buffer, closedTag);
-                if (closedTagPos == NULL)
-                {
-                    while (fgets(buffer, 99, fPtr) != NULL)
-                    {
-                        closedTagPos = substr(buffer, closedTag);
-                        if(closedTagPos == NULL)
-                            continue;
-                        else
-                        {
-                            pos1 = 0;
-                            pos2 = closedTagPos - buffer + len(closedTag);
-                            buffer = delSubstr(buffer, pos1, pos2);
-                            break;
-                        }
-                    }
-                }
-                else
-                {
-                    pos2 = closedTagPos - buffer + len(closedTag);
-                    buffer = delSubstr(buffer, pos1, pos2);
-                }
-                tagPos = substr(buffer, fullTag);
-            }
-
-            printf("%s", buffer);
-        }
-        printf("\n");
-
-        fclose(fPtr);
-    }
-}
-
-int len(char* str)
+int len(const char* str)
 {
     int len = 0;
-    while (*str != '\0') {
-        len++;
-        str++;
-    }
+    for (int i = 0; str[i] != '\0'; i++) len++;
     return len;
 }
 
-void my_strcat(char* str1, const char* str2)
-{
-    int i = len(str1);
-    int j = 0;
-    while (str2[j] !='\0')
-    {
-        str1[i] = str2[j];
-        i++; j++;
+char* getWord(char* str) {
+    char* word = (char*)malloc(100 * sizeof(char));
+    char* wordStart = word;
+
+    for (; *str != '\n'; str++) {
+        for (; *str != ' '; str++, word++) {
+            if (*str == '\n') {
+                break;
+            }
+            *word = *str;
+        }
+        *word = '\0';
+        return wordStart;
     }
-    str1[i + 1] = '\0';
+    return wordStart;
+}
+/*
+int isSameLen(char* str)
+{
+    char* temp = (char*) malloc(100 * sizeof(char));
+    int length;
+    temp = getWord(str);
+    length = len(temp);
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        while (*str == ' ') str++;
+        if (*str == '\n' || *str == '\0') break;
+        temp = getWord(str);
+        if (len(temp) != length)
+        {
+            free(temp);
+            return 0;
+        }
+    }
+
+    free(temp);
+    return 1;
+}
+*/
+int stringsWithSameLenWords(const char* fName)
+{
+    FILE* fp = NULL;
+    char* buff = (char*)malloc(2048 * sizeof(char));
+    char* temp = (char*)malloc(100 * sizeof(char));
+    char* start;
+    int firstLength, length, flag;
+
+    fp = fopen(fName, "r");
+
+    if (fp == NULL)
+    {
+        printf("File has not been opened");
+        return 1;
+    }
+    else
+    {
+        while (fgets(buff, 2048, fp) != NULL)
+        {
+            /*if(isSameLen(buff))
+                printf("%s", buff);
+            else
+                continue;
+            */
+
+            start = buff;
+            temp = getWord(buff);
+            firstLength = len(temp);
+            buff = buff + firstLength;
+            flag = 1;
+
+            for (int i = 0; buff[i] != '\n'; i++)
+            {
+                while (*buff == ' ') buff++;
+                if (*buff == '\n' || *buff == '\0') break;
+
+                temp = getWord(buff);
+                length = len(temp);
+                if (length != firstLength)
+                {
+                    flag = 0;
+                    break;
+                }
+                buff = buff + length;
+            }
+
+            if (flag)
+                printf("%s", start);
+            buff = start;
+        }
+        free(buff);
+        free(temp);
+        return 0;
+    }
 }
 
-char* substr(char* str1, char* str2)
+int main()
 {
-	int n = len(str1), m = len(str2);
-	for (int i = 0; i < n - m; i++)
-	{
-        int j = 0;
-        for (; j < m; j++)
-            if (str1[i + j] != str2[j]) break;
-        if (j == m) return str1 + i;
-	}
-    return NULL;
-}
+    char fName[50] = "text.txt";
 
-char* delSubstr(char* str1, int start, int end)
-{
-    char* str2 = new char[98];
-    char* res = str2;
-    for (int i = 0; *str1 != '\0'; str1++, i++)
-    {
-        if (i >= start && i < end)
-            continue;
-        *str2 = *str1;
-        str2++;
-    }
-    *str2 = '\0';
-    return res;
+    stringsWithSameLenWords(fName);
 }
