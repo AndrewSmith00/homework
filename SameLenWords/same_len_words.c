d451ef652a90c8e654b45e78cf5fed9e2f0358d2
AndrewSmith00 <91675747+AndrewSmith00@users.noreply.github.com>
2022-04-27 21:39:04 +0300

    Update same_len_words.c

--- SameLenWords/same_len_words.c
+++ SameLenWords/same_len_words.c
@@ -1,61 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#pragma warning(disable : 4996) 
 
-int len(const char *str)
+int len(const char* str)
 {
-    int i = 0;
-    for(; str[i] != '\0'; i++) {}
-    return i; 
+    int len = 0;
+    for (int i = 0; str[i] != '\0'; i++) len++;
+    return len;
 }
 
 char* getWord(char* str) {
-	char* word = (char*) malloc(100 * sizeof(char));
-	char* wordStart = word;
+    char* word = (char*)malloc(100 * sizeof(char));
+    char* wordStart = word;
 
-	for (; *str != '\n'; str++) {
-		for (; *str != ' '; str++, word++) {
-			if (*str == '\n') {
-				break;
-			}
-			*word = *str;
-		}
-		*word = '\0';
-		return wordStart;
-	}
+    for (; *str != '\n'; str++) {
+        for (; *str != ' '; str++, word++) {
+            if (*str == '\n') {
+                break;
+            }
+            *word = *str;
+        }
+        *word = '\0';
+        return wordStart;
+    }
     return wordStart;
 }
 /*
-int isSameLen(char* str) 
+int isSameLen(char* str)
 {
     char* temp = (char*) malloc(100 * sizeof(char));
     int length;
-
     temp = getWord(str);
     length = len(temp);
-
     for (int i = 0; str[i] != '\0'; i++)
-	{
-		while (*str == ' ') str++;
-		if (*str == '\n' || *str == '\0') break;
-
+    {
+        while (*str == ' ') str++;
+        if (*str == '\n' || *str == '\0') break;
         temp = getWord(str);
-        if (len(temp) != length) 
+        if (len(temp) != length)
         {
             free(temp);
             return 0;
         }
     }
-    
+
     free(temp);
     return 1;
 }
 */
-int stringsWithSameLenWords(const char *fName)
+int stringsWithSameLenWords(const char* fName)
 {
     FILE* fp = NULL;
-    char* buff = (char*) malloc(2048 * sizeof(char));
-    char* temp = (char*) malloc(100 * sizeof(char));
-    int length, flag;
+    char* buff = (char*)malloc(2048 * sizeof(char));
+    char* temp = (char*)malloc(100 * sizeof(char));
+    char* start;
+    int firstLength, length, flag;
 
     fp = fopen(fName, "r");
 
@@ -63,19 +62,21 @@ int stringsWithSameLenWords(const char *fName)
     {
         printf("File has not been opened");
         return 1;
-    } 
+    }
     else
     {
-        while(fgets(buff, 2048, fp) != NULL)
+        while (fgets(buff, 2048, fp) != NULL)
         {
-            /*if(isSameLen(buff)) 
+            /*if(isSameLen(buff))
                 printf("%s", buff);
             else
                 continue;
             */
 
+            start = buff;
             temp = getWord(buff);
-            length = len(temp);
+            firstLength = len(temp);
+            buff = buff + firstLength;
             flag = 1;
 
             for (int i = 0; buff[i] != '\n'; i++)
@@ -84,17 +85,19 @@ int stringsWithSameLenWords(const char *fName)
                 if (*buff == '\n' || *buff == '\0') break;
 
                 temp = getWord(buff);
-                if (len(temp) != length) 
+                length = len(temp);
+                if (length != firstLength)
                 {
                     flag = 0;
                     break;
                 }
+                buff = buff + length;
             }
 
             if (flag)
-                printf("%s", buff);
+                printf("%s", start);
+            buff = start;
         }
-
         free(buff);
         free(temp);
         return 0;
