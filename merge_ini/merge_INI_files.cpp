50f54d80c26fcb2358c890924873ebb60861b62e
AndrewSmith00 <91675747+AndrewSmith00@users.noreply.github.com>
2022-05-02 15:38:58 +0300

    Add files via upload

---
+++ merge_ini/merge_INI_files.cpp
@@ -0,0 +1,118 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable : 4996)
+
+int strLength(const char* str)
+{
+    int len = 0;
+    for (int i = 0; str[i] != '\0'; i++) len++;
+    return len;
+}
+
+void strCopy(char* str1, char* str2)
+{
+    int i, len;
+
+    len = strLength(str2);
+    for (i = 0; i < len; i++)
+    {
+        str1[i] = str2[i];
+    }
+    str1[i] = '\0';
+}
+
+char* findSubstr(char* str, const char* substr)
+{
+    int n, m, j;
+    n = strLength(str);
+    m = strLength(substr);
+
+    for (int i = 0; i < n; i++)
+    {
+        j = 0;
+        for (; j < m; j++)
+        {
+            if (str[i] != substr[j]) break;
+            i++;
+        }
+        if (j == m)
+            return (str + i - j);
+    }
+    return NULL;
+}
+
+void mergeINIfiles(const char *firstFileName, const char *scndFileName)
+{
+    FILE* fPointer1 = NULL;
+    FILE* fPointer2 = NULL;
+    char* str; //* sectionName;
+    char** checkedSectionNames;
+    int k;
+
+    fPointer1 = fopen(firstFileName, "r");
+    //fPointer2 = fopen(scndFileName, "r");
+    str = (char*)malloc(99 * sizeof(char));
+    checkedSectionNames = (char**)malloc(99 * sizeof(char*));
+    k = 0;
+
+    for (int i = 0; i < 99; i++)
+        checkedSectionNames[i] = (char*)malloc(99 * sizeof(char));
+
+    while (fgets(str, 99, fPointer1) != NULL)
+    {
+        str[strLength(str) - 1] = '\0';
+        printf("%s\n", str);
+        if (findSubstr(str, "[") != NULL && findSubstr(str, "]") != NULL)
+        {
+            strCopy(checkedSectionNames[k], str);
+
+            while (fgets(str, 99, fPointer1) != NULL)
+            {
+                if(findSubstr(str, "[") != NULL && findSubstr(str, "]") != NULL) break;
+                str[strLength(str) - 1] = '\0';
+                printf("%s\n", str);
+            }
+
+            fPointer2 = fopen(scndFileName, "r");
+
+            while (fgets(str, 99, fPointer2) != NULL)
+            {
+                str[strLength(str) - 1] = '\0';
+                if (findSubstr(str, checkedSectionNames[k]) != NULL)
+                {
+                    if (fgets(str, 99, fPointer2) == NULL) break;
+                    str[strLength(str) - 1] = '\0';
+                    if (findSubstr(str, "[") != NULL && findSubstr(str, "]") != NULL) break;
+
+                    printf("%s\n", str);
+                }
+
+                    
+            }
+
+            k++;
+
+            fclose(fPointer2);
+        }
+    }
+
+    for (int i = 0; i < 99; i++)
+    {
+        free(checkedSectionNames[i]);
+    }
+
+    free(checkedSectionNames);
+    free(str);
+
+    fclose(fPointer1);
+}
+
+int main()
+{
+    char firstFileName[90] = "test_first.ini";
+    char scndFileName[90] = "test_second.ini";
+
+    mergeINIfiles(firstFileName, scndFileName);
+
+    return 0;
+}
