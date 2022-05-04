1f195a7de1bc749503fafdf18714839f2de29dec
AndrewSmith00 <91675747+AndrewSmith00@users.noreply.github.com>
2022-05-04 23:06:47 +0300

    Update CreateDataBase.cpp

--- CreateDataBase.cpp
+++ CreateDataBase.cpp
@@ -1,9 +1,6 @@
-﻿#include <iostream>
 #include <stdio.h>
-#include <windows.h>
-#pragma warning(disable : 4996) 
-
-using namespace std;
+#include <iostream>
+#include <cstring>
 
 struct Student
 {
@@ -14,38 +11,90 @@ struct Student
 	int adress, groupNumber, booksNumber, booksValue;
 };
 
-int setStudentInfo(char* fileName);
+int setStudentInfo(char* filename);
+int getStudentInfo(char* filename, Student* arrOfStudents, long &count);
+void sortByGroups(char* filename);
 
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+	char filename[100] = "source.bin";
 
-	char fileName[100] = { "C:/Users/aokuznetsov/source/repos/BinaryFiles/BinaryFiles/source.bin" };
-
-	setStudentInfo(fileName);
+	setStudentInfo(filename);
 }
 
-int setStudentInfo(char* fileName)
+int setStudentInfo(char* filename)
 {
-	FILE* fPtr = fopen(fileName, "wb");
+	FILE* fPtr = fopen(filename, "wb");
 	struct Student student;
-	int count;
 
 	if (!fPtr) {
 		printf("Ошибка при создании файла");
 		return -1;
 	}
 	else {
-		printf("Вводите данные о студентах. Для завершения введите - - - - 0 0 0 0\n");
-		scanf("%s%s%s%s%d%d%d%d", student.surname, student.name, student.secondname, student.adressStreet, &student.adress, &student.groupNumber, &student.booksNumber, &student.booksValue);
+		printf("Вводите данные о студентах. Для завершения введите 0 - - - - 0 0 0\n");
+		printf("№ группы | Фамилия | Имя | Отчество | Улица | Дом | Количество книг | Стоимость книг\n");
+		scanf("%d%s%s%s%s%d%d%d", &student.groupNumber, student.surname, student.name, student.secondname, student.adressStreet, &student.adress, &student.booksNumber, &student.booksValue);
 
 		while(strcmp(student.surname, "-") != 0)
 		{
 			fwrite(&student, sizeof(struct Student), 1, fPtr);
-			scanf("%s%s%s%s%d%d%d%d", student.surname, student.name, student.secondname, student.adressStreet, &student.adress, &student.groupNumber, &student.booksNumber, &student.booksValue);
+			scanf("%d%s%s%s%s%d%d%d", &student.groupNumber, student.surname, student.name, student.secondname, student.adressStreet, &student.adress, &student.booksNumber, &student.booksValue);
 		}
+		
+		sortByGroups(filename);
+		return 0;
+	}
+}
+
+int getStudentInfo(char* filename, Student* arrOfStudents, long &count)
+{
+	FILE* fPtr = fopen(filename, "rb");
+	count = 0;
+
+	if(!fPtr)
+	{
+		printf("Ошибка при открытии файла");
+		return -1;
+	}
+	else
+	{
+		while (fread(&arrOfStudents[count], sizeof(struct Student), 1, fPtr) != 0)
+		{
+			count++;
+			fseek(fPtr, count * sizeof(struct Student), 0);
+		}
+
 		fclose(fPtr);
 		return 0;
 	}
+}
+
+void sortByGroups(char* filename)
+{
+	FILE* fPtr;
+	Student* arrOfStudents = new Student[999];
+	Student temp;
+	long count;
+
+	getStudentInfo(filename, arrOfStudents, count);
+
+	for (int i = 0; i < count - 1; i++)
+		for (int j = 0; j < count - i - 1; j++)
+			if (arrOfStudents[j].groupNumber > arrOfStudents[j+1].groupNumber)
+			{
+				temp = arrOfStudents[j];
+				arrOfStudents[j] = arrOfStudents[j+1];
+				arrOfStudents[j+1] = temp;
+			}
+
+	fPtr = fopen(filename, "wb");
+
+	for(int i = 0; i < count; i++)
+	{
+		fseek(fPtr, i * sizeof(struct Student), 0);
+		fwrite(&arrOfStudents[i], sizeof(struct Student), 1, fPtr);
+	}
+
+	delete arrOfStudents;
 }
