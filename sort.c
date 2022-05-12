01ebc26047fd13bb6ca32af8c501bca30e23c9f8
AndrewSmith00 <91675747+AndrewSmith00@users.noreply.github.com>
2022-05-12 21:07:01 +0300

    Update sort.c

--- sort.c
+++ sort.c
@@ -5,96 +5,85 @@ int const n = 999;
 
 int main()
 {
-    int arr[n];
-    int maxCount = 1, maxIndex = -1, count;
+	int arr[n];
+	int maxCount = 1, maxIndex = -1, count;
 
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = rand()%100;
-    }
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
 
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
 
-    for (int i = 0; i < n; i++)
-    {
-        count = 0;
-        for (int j = i; j < n; j++)
-        if (arr[i] == arr[j])
-            count++;
+	for (int i = 0; i < n; i++)
+	{
+		count = 0;
+		for (int j = i; j < n; j++)
+			if (arr[i] == arr[j])
+				count++;
 
-        if (maxCount < count)
-        {
-            maxCount = count;
-            maxIndex = i;
-        }
-    }
+		if (maxCount < count)
+		{
+			maxCount = count;
+			maxIndex = i;
+		}
+	}
 
-    if(maxIndex > -1)
-        printf("Most frequent element is %d\n", arr[maxIndex]);
-    else
-        printf("No repeating elements");
-    // sorting:
-    double factor = 1.3;
-    int step, temp, countOp;
-    //int left, right;
+	if (maxIndex > -1)
+		printf("Most frequent element is %d\n", arr[maxIndex]);
+	else
+		printf("No repeating elements\n");
 
-    step = n / factor;
-    countOp = 0;
-    
-/*
-    while (step >= 1)
-    {
-        left = 0;
-        right = n - 1;
-        while (left < right)
-        {
-            for (int i = left; i + step < right; i++)
-            {   
-                if (arr[i] > arr[i + step])
-                {
-                    temp = arr[i];
-                    arr[i] = arr[i + step];
-                    arr[i + step] = temp;
-                    countOp++;
-                }
-            }
-            right--;
-            for (int i = right; i - step > left; i--)
-            {
-                if (arr[i] < arr[i - step])
-                {
-                    temp = arr[i];
-                    arr[i] = arr[i + step];
-                    arr[i + step] = temp;
-                    countOp++;
-                }
-            }
-            left++;
-        }
-        
-        step /= factor;
-    }
-    */
-    
-    while(step >= 1)
-    {
-        for (int i = 0; i + step < n; i++)
-            {
-                if (arr[i] > arr[i + step])
-                {   temp = arr[i];
-                    arr[i] = arr[i + step];
-                    arr[i + step] = temp;
-                    countOp++;
-                }
-            }
-        step /= factor;
-    }
-    
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\nAmount of operations: %d\n", countOp);
+	// sorting:
+	double factor;
+	int step, temp, left, right, control, swapped;
 
-    return 0;
+	swapped = 1;
+	step = n;
+	left = 0;
+	right = n - 1;
+	factor = 1.3;
+
+	while (step > 1 || swapped)
+	{
+		if (step > 1)
+			step = step / factor;
+		swapped = 0;
+
+
+		for (int i = left; i + step <= right; i++)
+		{
+			control = right;
+			if (arr[i] > arr[i + step])
+			{
+				temp = arr[i];
+				arr[i] = arr[i + step];
+				arr[i + step] = temp;
+				swapped = 1;
+				control = i;
+			}
+		}
+		control = right;
+
+		for (int i = right; i - step >= left; i--)
+		{
+			control = left;
+			if (arr[i] < arr[i - step])
+			{
+				temp = arr[i];
+				arr[i] = arr[i - step];
+				arr[i - step] = temp;
+				swapped = 1;
+				control = i;
+			}
+		}
+		left = control;
+	}
+
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+
+	return 0;
 }
