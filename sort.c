8ae032154d16c8d2b63754e30e9af9b283e5803c
AndrewSmith00 <91675747+AndrewSmith00@users.noreply.github.com>
2022-05-12 22:22:06 +0300

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,89 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int const n = 999;
+int const n = 9999;
 
 int main()
 {
-	int arr[n];
-	int maxCount = 1, maxIndex = -1, count;
+    int arr[n];
+    int maxCount = 1, maxIndex = -1, count;
+    clock_t start, stop;
+    double duration;
 
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 100;
-	}
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 10;
+    }
+    printf("\nUnsorted array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n\n");
 
-	for (int i = 0; i < n; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
+    //--------------------------------Find most frequent (stupid version)------------------------------
 
-	for (int i = 0; i < n; i++)
-	{
-		count = 0;
-		for (int j = i; j < n; j++)
-			if (arr[i] == arr[j])
-				count++;
+    start = clock();
 
-		if (maxCount < count)
-		{
-			maxCount = count;
-			maxIndex = i;
-		}
-	}
+    for (int i = 0; i < n; i++)
+    {
+        count = 0;
+        for (int j = i; j < n; j++)
+            if (arr[i] == arr[j])
+                count++;
 
-	if (maxIndex > -1)
-		printf("Most frequent element is %d\n", arr[maxIndex]);
-	else
-		printf("No repeating elements\n");
+        if (maxCount < count)
+        {
+            maxCount = count;
+            maxIndex = i;
+        }
+    }
 
-	// sorting:
-	double factor;
-	int step, temp, left, right, control, swapped;
+    stop = clock();
 
-	swapped = 1;
-	step = n;
-	left = 0;
-	right = n - 1;
-	factor = 1.3;
+    duration = (double)(stop - start) / CLOCKS_PER_SEC;
 
-	while (step > 1 || swapped)
-	{
-		if (step > 1)
-			step = step / factor;
-		swapped = 0;
+    if (maxIndex > -1)
+        printf("Most frequent element is %d\nTime for unsorted array: %2.15f sec\n\n", arr[maxIndex], duration);
+    else
+        printf("No repeating elements\nTime for unsorted array: %2.15f sec\n\n", duration);
 
+    // ----------------------------------Sorting:------------------------------------------------------
 
-		for (int i = left; i + step <= right; i++)
-		{
-			control = right;
-			if (arr[i] > arr[i + step])
-			{
-				temp = arr[i];
-				arr[i] = arr[i + step];
-				arr[i + step] = temp;
-				swapped = 1;
-				control = i;
-			}
-		}
-		control = right;
+    start = clock();
+    
+    double factor;
+    int step, temp, swapped;
 
-		for (int i = right; i - step >= left; i--)
-		{
-			control = left;
-			if (arr[i] < arr[i - step])
-			{
-				temp = arr[i];
-				arr[i] = arr[i - step];
-				arr[i - step] = temp;
-				swapped = 1;
-				control = i;
-			}
-		}
-		left = control;
-	}
+    swapped = 1;
+    step = n;
+    factor = 1.3;
 
-	for (int i = 0; i < n; i++)
-		printf("%d ", arr[i]);
+    while (step > 1 || swapped)
+    {
+        if (step > 1)
+            step = step / factor;
+        swapped = 0;
 
-	return 0;
+        for (int i = 0; i + step < n; i++)
+        {
+            if (arr[i] > arr[i + step])
+            {
+                temp = arr[i];
+                arr[i] = arr[i + step];
+                arr[i + step] = temp;
+                swapped = 1;
+            }
+        }
+        for (int i = n - 1; i - step >= 0; i--)
+        {
+            if (arr[i] < arr[i - step])
+            {
+                temp = arr[i];
+                arr[i] = arr[i - step];
+                arr[i - step] = temp;
+                swapped = 1;
+            }
+        }
+    }
+
+    //----------------------------------------Find most frequent (cool version)-------------------------------
+
+    int last, save;
+    count = 1;
+    maxCount = 1;
+
+    last = arr[0];
+    save = last;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] == last)
+            count++;
+        else
+        {
+            if (maxCount < count)
+            {
+                maxCount = count;
+                save = last;
+            }
+            last = arr[i];
+            count = 1;
+        }
+    }
+    if (maxCount < count)
+    {
+        maxCount = count;
+        save = last;
+    }
+
+    stop = clock();
+
+    duration = (double)(stop - start) / CLOCKS_PER_SEC;
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n\n");
+
+    printf("Most frequent element is %d\nTime for sorted array:   %2.15f sec\n\n", save, duration);
+
+    return 0;
 }
